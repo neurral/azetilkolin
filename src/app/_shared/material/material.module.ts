91159b028c6dcfed465d5e9553b3360b382ca795
Lee Alexis <john20xdoe@users.noreply.github.com>
2019-09-01 10:09:20 +0800

    Update: added material

--- src/app/_shared/material/material.module.ts
+++ src/app/_shared/material/material.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatButtonModule, MatCheckboxModule } from "@angular/material";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+const materialModules = [
+  BrowserAnimationsModule,
+  MatButtonModule,
+  MatCheckboxModule
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule
-  ]
+  imports: [CommonModule, ...materialModules],
+  exports: materialModules
 })
 export class MaterialModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SharedModule } from "./_shared/shared/shared.module";
 import { MaterialModule } from "./_shared/material/material.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule, SharedModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
