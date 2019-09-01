019a5749365491b6786a92ba87e0b8015903248e
Lee Alexis <john20xdoe@users.noreply.github.com>
2019-09-01 11:15:08 +0800

    Modules organization

--- src/app/_shared/material/material.module.ts
+++ src/app/_shared/material/material.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
 
-const materialModules = [
-  BrowserAnimationsModule,
-  MatButtonModule,
-  MatCheckboxModule
-];
+const materialModules = [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule];
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, ...materialModules],
-  exports: materialModules
+  exports: materialModules,
 })
 export class MaterialModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { SharedModule } from "./_shared/shared/shared.module";
-import { MaterialModule } from "./_shared/material/material.module";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './_shared/shared.module';
+import { MaterialModule } from './_shared/material/material.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, MaterialModule, SharedModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
